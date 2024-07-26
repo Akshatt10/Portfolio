b55d595682ed7e9f9d88f9a6527fb0021ffe065c
Akshat Tyagi <95146187+Akshatt10@users.noreply.github.com>
2024-07-26 18:12:40 +0530

    Update Projects.jsx

--- Portfolio/src/components/Projects.jsx
+++ Portfolio/src/components/Projects.jsx
@@ -18,8 +18,8 @@ const Projects = () => {
                 className='w-full lg:w-1/4'>
                 <img 
                 src={project.image}
-                width={200}
-                height={200}
+                width={250}
+                height={250}
                 alt={project.title}
                 className='mb-6 rounded' />
                 </motion.div>
