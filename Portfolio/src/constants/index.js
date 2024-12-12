580a20c5eef4afea509057467e829230425bbe13
Akshatt10 <akshattyagi1010@gmail.com>
2024-12-12 22:15:22 +0530

    project picture

--- Portfolio/src/constants/index.js
+++ Portfolio/src/constants/index.js
@@ -1,4 +1,4 @@
-import project1 from "../assets/projects/project-1.png";
+import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.png";
 import project3 from "../assets/projects/project-3.png";
 import project4 from "../assets/projects/project-4.png";
